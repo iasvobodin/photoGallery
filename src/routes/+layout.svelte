f9ef14b450601fe99f3016af830b897966a355fe
iasvobodin <iasvobodin@gmail.com>
2023-02-28 22:07:06 +0500

    22

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,6 @@
 	import { wWidth } from '$lib/store.js';
 	import { wHeight } from '$lib/store.js';
 	import { onMount } from 'svelte';
-
-	// console.log($wWidth, $wHeight);
-	onMount(() => {
-		// console.log('layout is mounting');
-	});
 </script>
 
 <slot />
