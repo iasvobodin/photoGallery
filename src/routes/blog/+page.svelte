13a71258cceeac89ca74be28a44931ea55007898
iasvobodin <iasvobodin@gmail.com>
2023-04-22 20:25:54 +0500

    blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,24 +1,65 @@
 <script>
 	import { onMount } from 'svelte';
 
-	// let VK;
-	// import * as VK from '$lib/assets/openapi.js';
 	onMount(() => {
-		window.VK.init({
+		VK.init({
 			apiId: '51622968',
 			onlyWidgets: true
 		});
-		console.log(window.VK);
-		window.VK.Widgets.Article(
-			'article_view_-163390211_40723',
-			'/@svobodinaphoto-zagsy-naiznanku-sovetskii-raion'
+		VK.Widgets.Article(
+			'vk_article_-163390211_40723',
+			'@svobodinaphoto-zagsy-naiznanku-sovetskii-raion'
+		);
+		VK.Widgets.Article(
+			'vk_article_-163390211_81528',
+			'@svobodinaphoto-zagsy-naiznanku-kurchatovskii-raion'
+		);
+		VK.Widgets.Article(
+			'vk_article_-163390211_60470',
+			'@svobodinaphoto-zagsy-naiznanku-centralnyi-raion'
+		);
+		VK.Widgets.Article(
+			'vk_article_-163390211_44003',
+			'@svobodinaphoto-zagsy-naiznanku-kalininskii-raion'
+		);
+		VK.Widgets.Article(
+			'vk_article_-163390211_89413',
+			'@svobodinaphoto-kupit-gotovoe-plate-ili-sshit-svoe'
+		);
+		VK.Widgets.Article(
+			'vk_article_-163390211_31609',
+			'@svobodinaphoto-chelyabinskie-zagsy-glazami-hudozhnika-photoshop-i-nikakogo'
 		);
 	});
 </script>
 
 <svelte:head>
 	<title>Статьи</title>
-	<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
+	<script
+		type="text/javascript"
+		src="https://vk.com/js/api/openapi.js?168"
+		charset="windows-1251"
+	></script>
 </svelte:head>
+<h1 class="main__head">Статьи</h1>
+<div class="articles">
+	<div id="vk_article_-163390211_40723" />
+	<div id="vk_article_-163390211_81528" />
+	<div id="vk_article_-163390211_60470" />
+	<div id="vk_article_-163390211_44003" />
+	<div id="vk_article_-163390211_89413" />
+	<div id="vk_article_-163390211_31609" />
+</div>
 
-<div id="article_view_-163390211_40723" />
+<style>
+	.articles {
+		width: max(1000px, 95%);
+		display: grid;
+		justify-content: center;
+		grid-template-columns: repeat(auto-fill, minmax(max(35vw, 250px), 1fr));
+		row-gap: 5vw;
+		column-gap: 5vw;
+		margin: auto;
+		padding-bottom: 10vh;
+	}
+</style>
