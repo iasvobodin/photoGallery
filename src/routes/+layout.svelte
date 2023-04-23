983a822c20da556dabe2b5dbdf75c40e91604f49
iasvobodin <iasvobodin@gmail.com>
2023-04-24 02:42:19 +0500

    !!!!!!!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,8 @@
 	import { dev } from '$app/environment';
 
 
-console.log($page.url);
+let canonical = `https://svobodinaphoto.ru${$page.url.pathname}`
+// console.log(canonical);
 
 	let numbers: Array<number> = [];
 	const stileRandom = (i: number) => {
@@ -108,7 +109,7 @@ console.log($page.url);
 		content="Профессиональный фотограф Свободина Анастасия Челябинск. Свадебная, портретная, репортажная, семейная фотосессии. Профессиональное оборудование, быстрые сроки, адекватные цены"
 	/>
 	<meta property="vk:image" content="https://img.svobodinaphoto.ru/1024_19-08-24-17-09-02.jpg" />
-	<link rel="canonical" href="https://svobodinaphoto.ru{$page.url.pathname}" />
+	<link rel="canonical" href={canonical} />
 	<!-- {#if !dev}
 		<script type="text/javascript" src="/metrika.js"></script>
 	{/if} -->
