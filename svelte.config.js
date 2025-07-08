6ce3380fc12b06257272eb2b8b4597ffc7d05f97
iasvobodin <iasvobodin@gmail.com>
2025-07-08 23:07:28 +0500

    Фикс ошибок и обновления для тестирования

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
 import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+// import { vitePreprocess } from '@sveltejs/kit';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
