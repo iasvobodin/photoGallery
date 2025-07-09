7d1267a355ebd830d374fd520be467624a73000a
Ivan <51612499+iasvobodin@users.noreply.github.com>
2025-07-09 10:29:27 +0500

    restore csp

--- svelte.config.js
+++ svelte.config.js
@@ -4,38 +4,39 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 // import { vitePreprocess } from '@sveltejs/kit';
 /** @type {import('@sveltejs/kit').Config} */
-
-
-
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      precompress: false,
-      strict: true
-    }),
-    csp: {
-      directives: {
-        'script-src': [
-          '"self"',  // Исправлено: добавлены кавычки
-          'https://yastatic.net',  // Добавлен https://
-          'https://mc.yandex.ru',  // Добавлен https://
-          '"unsafe-inline"'
-        ],
-        'connect-src': [
-          '"self"',
-          'https://mc.yandex.ru'  // Для отправки данных
-        ],
-        'img-src': [
-          '"self"',
-          'data:',
-          'https://mc.yandex.ru'  // Для пикселя Метрики
-        ]
-      }
-    }
-  }
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter: adapter({
+		// 	// if true, will create a Netlify Edge Function rather
+		// 	// than using standard Node-based functions
+		// 	edge: false,
+
+		// 	// if true, will split your app into multiple functions
+		// 	// instead of creating a single one for the entire app.
+		// 	// if `edge` is true, this option cannot be used
+		// 	split: false
+		// }),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			// fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		// csp: {
+		// 	mode: 'auto',
+		// 	directives: {
+		// 		'child-src': ['blob:', 'https://mc.yandex.ru'],
+		// 		'frame-src': ['blob:', 'https://mc.yandex.ru'],
+		// 		'img-src': ['https://mc.yandex.ru', 'localhost:*', 'https:'],
+		// 	}
+		// }
+	}
 };
 
 export default config;
