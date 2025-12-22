7a360372f3a9c05e1f24884db575536f52b6118c
iasvobodin <iasvobodin@gmail.com>
2025-12-22 10:45:10 +0500

    поместил метрику в onMount надо проверить как работает

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { browser, dev } from '$app/environment';
+	import { onMount } from 'svelte';
 	import Logo from '$lib/components/logo.svelte';
 	import Lenis from '@studio-freight/lenis';
 
@@ -99,6 +100,38 @@
 		console.log(menuIsOpen);
 		menuIsOpen ? lenis.stop() : lenis.start();
 	};
+
+	onMount(() => {
+		if (!browser) return;
+
+		// Предотвращаем повторную инициализацию
+		if (window.ym) return;
+
+		// Вставляем скрипт Метрики
+		(function (m, e, t, r, i, k, a) {
+			m[i] =
+				m[i] ||
+				function () {
+					(m[i].a = m[i].a || []).push(arguments);
+				};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) return;
+			}
+			k = e.createElement(t);
+			a = e.getElementsByTagName(t)[0];
+			k.async = 1;
+			k.src = r;
+			a.parentNode.insertBefore(k, a);
+		})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+		window.ym(47422762, 'init', {
+			clickmap: true,
+			trackLinks: true,
+			accurateTrackBounce: true
+			// webvisor: true, // если нужен вебвизор
+		});
+	});
 </script>
 
 <svelte:window bind:scrollY={y} bind:scrollX={x} />
@@ -136,7 +169,7 @@
     <!-- End Cloudflare Web Analytics -->
   `}
 
-		<script type="text/javascript">
+		<!-- <script type="text/javascript">
 			(function (m, e, t, r, i, k, a) {
 				m[i] =
 					m[i] ||
@@ -161,7 +194,7 @@
 				trackLinks: true,
 				accurateTrackBounce: true
 			});
-		</script>
+		</script> -->
 		<noscript
 			><div>
 				<img
